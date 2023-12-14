2e926cbda7215913097210c11447557c17d59779
yousef-15 <115038881+yousef-15@users.noreply.github.com>
2023-12-14 23:26:21 +0200

    Update Integrated_Game.cpp

--- Integrated program/Integrated_Game.cpp
+++ Integrated program/Integrated_Game.cpp
@@ -20,82 +20,81 @@ int main()
         cin >> choice;
         switch (choice)
         {
-        case 1:
-        {
-            Player *players[2];
-            players[0] = new Player(1, 'x');
-            cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
-            cin >> choice;
-            if (choice != 1)
-                players[1] = new Player(2, 'o');
-            else
-                // Player pointer points to child
-                players[1] = new RandomPlayer('o', 3);
-            GameManager x_o_game(new X_O_Board(), players);
-            x_o_game.run();
-            system("pause");
-            break;
-        }
-        case 2:
-        {
-            Player *players[2];
-            players[0] = new Player(1, 'x');
-            cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
-            cin >> choice;
-            if (choice != 1)
-                players[1] = new Player(2, 'o');
-            else
-                // Player pointer points to child
-                players[1] = new RandomPlayer('o', 5);
-            GameManager x_o_game(new X_O_Board5x5(), players);
-            x_o_game.run();
-            system("pause");
-            break;
-        }
-        case 3:
-        {
-            Player *players[2];
-            players[0] = new Player(1, 'x');
-            cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
-            cin >> choice;
-            if (choice != 1)
-                players[1] = new Player(2, 'o');
-            else
-                // Player pointer points to child
-                players[1] = new RandomPlayer('o', 3);
-            GameManager x_o_game(new Pyramic_X_O(), players);
-            x_o_game.run();
-            system("pause");
-            break;
-        }
-        case 4:
-        {
-            Player *players[2];
-            players[0] = new Player(1, 'x');
-            cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
-            cin >> choice;
-            if (choice != 1)
-                players[1] = new Player(2, 'o');
-            else
-                // Player pointer points to child
-                players[1] = new RandomPlayer('o', 7);
-            GameManager x_o_game(new Connect_four_board(), players);
-            x_o_game.run_connect();
-            system("pause");
-            break;
-        }
-        case 5:
-        {
-            cout << "Thank you for playing!\n";
-            return 0;
-        }
-        default:
-        {
-            cout << "Invalid choice. Please enter a number between 1 and 5.\n";
-            break;
-        }
+            case 1:
+            {
+                Player *players[2];
+                players[0] = new Player(1, 'x');
+                cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
+                cin >> choice;
+                if (choice != 1)
+                    players[1] = new Player(2, 'o');
+                else
+                    // Player pointer points to child
+                    players[1] = new RandomPlayer('o', 3);
+                GameManager x_o_game(new X_O_Board(), players);
+                x_o_game.run();
+                system("pause");
+                break;
+            }
+            case 2:
+            {
+                Player *players[2];
+                players[0] = new Player(1, 'x');
+                cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
+                cin >> choice;
+                if (choice != 1)
+                    players[1] = new Player(2, 'o');
+                else
+                    // Player pointer points to child
+                    players[1] = new RandomPlayer('o', 5);
+                GameManager x_o_game(new X_O_Board5x5(), players);
+                x_o_game.run5x5();
+                system("pause");
+                break;
+            }
+            case 3:
+            {
+                Player *players[2];
+                players[0] = new Player(1, 'x');
+                cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
+                cin >> choice;
+                if (choice != 1)
+                    players[1] = new Player(2, 'o');
+                else
+                    // Player pointer points to child
+                    players[1] = new RandomPlayer('o', 3);
+                GameManager x_o_game(new Pyramic_X_O(), players);
+                x_o_game.run();
+                system("pause");
+                break;
+            }
+            case 4:
+            {
+                Player *players[2];
+                players[0] = new Player(1, 'x');
+                cout << "Press 1 if you want to play with computer or 2 to play with your friend: ";
+                cin >> choice;
+                if (choice != 1)
+                    players[1] = new Player(2, 'o');
+                else
+                    // Player pointer points to child
+                    players[1] = new RandomPlayer('o', 7);
+                GameManager x_o_game(new Connect_four_board(), players);
+                x_o_game.run_connect();
+                system("pause");
+                break;
+            }
+            case 5:
+            {
+                cout << "Thank you for playing!\n";
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid choice. Please enter a number between 1 and 5.\n";
+                break;
+            }
         }
     }
-
     return 0;
 }
